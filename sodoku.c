712833cf188e291242ccb8378fbdd30e1b35b817
ForsakenPanda <nicholas.gregorio@uoit.net>
2015-10-23 11:49:13 -0400

    Reads from file, tests for printing as well

--- sodoku.c
+++ sodoku.c
@@ -4,9 +4,23 @@
 #include <unistd.h>
 
 //TEST CODE//
+int sudoku[9][9];
+
+void readPuzzle(FILE *file) {
+	for (int i = 0; i<9; i++) {
+			for (int j = 0; j<9; j++) {
+				fscanf(file, "%d", &sudoku[i][j]);
+			}
+		}
+
+}
 
 int main() {
-	printf("casual is working");
 
+	FILE *puzzle = fopen("puzzle.txt", "r");
+	readPuzzle(puzzle);
+
+	printf("%d", sudoku[0][0]);
+	printf("%d", sudoku[5][5]);
 	return 0;
 }
