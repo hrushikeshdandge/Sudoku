4ceb4998831cef4444c84f16c6a62a73fcb0aa0e
ForsakenPanda <nicholas.gregorio@uoit.net>
2015-10-30 12:49:14 -0400

    Completed Sudoku

--- sodoku.c
+++ sodoku.c
@@ -11,7 +11,7 @@ typedef struct {
 	int column,row;
 } box_coordinates;
 
-int correct[3] = { 0, 0, 0 };
+//Read the file
 void readPuzzle(FILE *file) {
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
@@ -20,8 +20,10 @@ void readPuzzle(FILE *file) {
 	}
 
 }
+
+
 //confirms that a specified number is not already used in the given row, column, and 3x3 section
-//Code referenced from: http://codereview.stackexchange.com/questions/37430/sudoku-solver-in-c
+//The following code was referenced to assist in checking a given box: http://codereview.stackexchange.com/questions/37430/sudoku-solver-in-c
 int checkNum(int row, int column, int num){
 	//Determines the row and column start of the entry's box, setting them to a multiple of 3
 	int rStart = (row/3) * 3;
@@ -40,30 +42,26 @@ int checkNum(int row, int column, int num){
 		if (sudoku[i][column] == num) {
 			flag = 0;
 		}
-		//If number appears in the box, it is unavailable.
-		if (sudoku[rStart+ (i%3)][cStart+(i/3)] == num) {
-			flag = 0;
+
+
+		//Iterate using rStart and cStart to check the specific box the number appears in
+		for (int j = rStart; j< rStart+3; j++){
+			for (int k = cStart; k < 3; k++) {
+				if (sudoku[j][k] == num ) {
+					flag = 0;
+
+				}
+			}
 		}
 	}
 	return flag;
 
 }
-//code referenced from http://codereview.stackexchange.com/questions/37430/sudoku-solver-in-c
+//The following code was referenced to assist in recursion / backtracking: http://codereview.stackexchange.com/questions/37430/sudoku-solver-in-c
 //Solves the sudoku problem
 int solver(int row, int col) {
-
-
-		//If current number is not 0, increment where possible
-		if (sudoku[row][col] != 0) {
-			if((row+1) < 9) {
-				return solver(row +1, col);
-			} else if((col+1) < 9) {
-				return solver(0, col+1);
-			} else {
-				return 1;
-			}
-		} else { //if the entry is 0
-
+		if (sudoku[row][col] == 0) {
+			//Entry is 0; determine values to place using a for loop
 			for (int i=1; i<=9; i++){
 				//Confirm the number is available
 				if (checkNum(row, col, i) == 1){
@@ -71,11 +69,13 @@ int solver(int row, int col) {
 					sudoku[row][col] = i;
 					//increment to the next row if possible
 					if ((row+1) < 9) {
+						//Ensure that the next position can be solved
 						if (solver(row + 1, col) == 1) {
 							return 1;
 						}
 
 						else {
+							//Reset value to 0. This allows for backtracking
 							sudoku[row][col] = 0;
 						}
 
@@ -85,6 +85,7 @@ int solver(int row, int col) {
 							return 1;
 						}
 						else {
+							//Again, if the next position does not solve, do not change this position so that backtracking can occur.
 							sudoku[row][col] = 0;
 						}
 					}
@@ -93,28 +94,39 @@ int solver(int row, int col) {
 					}
 				}
 			}
+		}
+		else { //current number not 0, increment where possible
+			if((row+1) < 9) {
+				return solver(row +1, col);
+			} else if((col+1) < 9) {
+				return solver(0, col+1);
+			} else {
+				return 1;
+			}
 		} return 0;
 
 }
 
 
 
-
-
 void *rowChecker() {
 	int passfail = 1;
-	for (int a = 0; a < 9; a++) { //goes through each row
+	//goes through each row
+	for (int a = 0; a < 9; a++) {
+		//Sets flags for each value in a row
 		int flags[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-		for (int i = 0; i < 9; i++) { //goes through each col
-			if (flags[sudoku[a][i] - 1] == 0) //goes through each value 1 - 9
+		//goes through each column
+		for (int i = 0; i < 9; i++) {
+			//If the number has not been used in the row, set the flag for that value to 1.
+			if (flags[sudoku[a][i] - 1] == 0)
 				flags[sudoku[a][i] - 1] = 1;
 			else {
+			//If the number has been used, break out of the loop
 				passfail = 0;
 				break;
 			}
 		}
 	}
-	printf("Rowcheck pass(1)/fail(0): %d\n", passfail);
 	if (passfail == 1) {
 		isCorrect[0] = 1;
 	}
@@ -123,21 +135,19 @@ void *rowChecker() {
 
 void *columnChecker() {
 	int incorrect = 0;
+	//Iterate through columns, one at a time
 	for (int column = 0; column < 9; column++) {
 		//Set array of flags
-		int flag[9];
-		for (int i = 0; i < 9; i++) {
-			flag[i] = 0;
-		}
+		int flag[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
+		//Iterate through rows, one at a time
 		for (int row = 0; row < 9; row++) {
+			//Set the current value into toCheck, and confirm that value does not occur already
 			int toCheck = sudoku[row][column];
 			if (flag[toCheck - 1] == 0) {
 				flag[toCheck - 1] = 1;
 			} else {
-
-				printf("Column incorrect; Row: %d Column: %d \n", row + 1,
-						column + 1);
+				//If the value already occurs, break out of the loop.
 				incorrect = 1;
 				break;
 			}
@@ -155,7 +165,6 @@ void *columnChecker() {
 	it used the box coordinates to offset the validator when checking the values
 */
 void *check_box(void * boxInfo) {
-
 	int value;
 	box_coordinates *coordinates = boxInfo;
 	int box_column = coordinates->column;
@@ -170,12 +179,10 @@ void *check_box(void * boxInfo) {
 	for (int column = 0; column < 3; column++) {
 		for (int row = 0; row < 3; row++) {
 			value = sudoku[row + box_row][column + box_column];
-			//printf("Value= %d\n", value);
+
 
 			//if the value has been found before, there is an error in the box
 			if (flags[value - 1] == 1) {
-				printf("ERROR IN ROW: %d COLUMN: %d\n", row + box_row + 1, column + box_column + 1);
-				printf("Value: %d", value);
 				error = 1;
 				break;
 
@@ -188,23 +195,18 @@ void *check_box(void * boxInfo) {
 		}
 		if (error == 0) {
 			isCorrect[2] = 1;
-		}
-	}
-	//prints out the location of the box if it has an error	
-	if (error == 1){
-		printf("ERROR IN BOX: Column: %d, Row: %d\n", box_row,
-			box_column);
 
-	}else
-		printf("Passed Box: Column: %d, Row: %d\n", box_row,
-			box_column);
+		}
+
+
+	}
 
 	return NULL;
 }
 
 void *validate(void) {
 		pthread_t pth;
-		pthread_create(&pth, 0, rowChecker, (void *) "Row Checker motherfuckers");
+		pthread_create(&pth, 0, rowChecker, (void *) "Row Checker");
 
 		pthread_t pth2;
 		pthread_create(&pth2, 0, columnChecker, (void *) "Column");
@@ -217,6 +219,7 @@ void *validate(void) {
 		int i = 0;
 		for (int countcolumn = 0; countcolumn <= 6; countcolumn += 3) {
 			for (int countrow = 0; countrow <= 6; countrow += 3) {
+				//Create a thread for each box to be checked
 				boxes[i].column = countcolumn;
 				boxes[i].row = countrow;
 				pthread_create(&boxThread[i], 0, check_box, (void *) &boxes[i]);
@@ -234,11 +237,14 @@ void *validate(void) {
 		return NULL;
 }
 
-int main() {
 
+int main() {
+	//Open the puzzle file and store the puzzle into a variable
 	FILE *puzzle = fopen("puzzle.txt", "r");
 	readPuzzle(puzzle);
+	FILE *solution = fopen("solution.txt", "w");
 
+	//Run the solver. If it returns 1, then the solution is found. Otherwise, it is not found
 	if (solver(0, 0)){
 		printf("Solution found\n");
 	}
@@ -247,21 +253,23 @@ int main() {
 	}
 	for (int i = 0; i < 9; i++){
 		for (int j = 0; j < 9; j++) {
-			printf("%d ", sudoku[i][j]);
+			fprintf(solution, "%d ", sudoku[i][j]);
 			if (j == 8) {
-				printf("\n");
+				fprintf(solution, "\n");
 			}
 		}
 	}
 
-/*
+
 	validate();
 	if (isCorrect[0] == 1 && isCorrect[1] == 1 && isCorrect[2] == 1){
 		printf("Sudoku is correct");
+	} else {
+		printf("Sudoku is incorrect");
 	}
 
 	return 0;
-	*/
+
 }
 
 
